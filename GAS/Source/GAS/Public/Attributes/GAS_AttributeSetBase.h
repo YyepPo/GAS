8454d25d12f7f7cc02e608dd1cc3368abff251cb
Enes Pacarizi <enespaqarizi23@gmail.com>
2026-04-08 16:28:55 +0100

    Initialize Attribute Set

--- GAS/Source/GAS/Public/Attributes/GAS_AttributeSetBase.h
+++ GAS/Source/GAS/Public/Attributes/GAS_AttributeSetBase.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "GAS_AttributeSetBase.generated.h"
 
 // Uses macros from AttributeSet.h
@@ -33,5 +34,71 @@ public:
 	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
 
 	/** Attribute set overrides END */
+
+	/** Current Health of the player.
+	* This variable is replicated.
+	* Capped by MaxHealth
+	*/
+	UPROPERTY(ReplicatedUsing = OnRep_Health, BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UGAS_AttributeSetBase, Health)
+
+	/** Max Health Amount. Capes Health amount
+	 * Can be updated during runtime.
+	 * This variable is replicated.
+	 */
+	UPROPERTY(ReplicatedUsing = OnRep_MaxHealth, BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UGAS_AttributeSetBase, MaxHealth)
+
+	/** Increase health amount every second.
+	 * Can be updated during runtime
+	 * This variable is replicated.
+	 */
+	UPROPERTY(ReplicatedUsing = OnRep_HealthRegenRate, BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData HealthRegenRate;
+	ATTRIBUTE_ACCESSORS(UGAS_AttributeSetBase, HealthRegenRate)
+	
+	/** Current Mana of the player.
+	* Used to execute abilities.
+	* This variable is replicated.
+	* Capped by MaxMana
+	*/
+	UPROPERTY(ReplicatedUsing = OnRep_Mana, BlueprintReadOnly, Category = "Mana")
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UGAS_AttributeSetBase, Mana)
+
+	/** Max Mana Amount. Capes Mana amount
+	 * Can be updated during runtime.
+	 * This variable is replicated.
+	 */
+	UPROPERTY(ReplicatedUsing = OnRep_MaxMana, BlueprintReadOnly, Category = "Mana")
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UGAS_AttributeSetBase, MaxMana)
+
+	/** Current Stamina of the player.
+	* Used to execute abilities.
+	* This variable is replicated.
+	* Capped by MaxStamina
+	*/
+	UPROPERTY(ReplicatedUsing = OnRep_Stamina, BlueprintReadOnly, Category = "Mana")
+	FGameplayAttributeData Stamina ;
+	ATTRIBUTE_ACCESSORS(UGAS_AttributeSetBase, Stamina)
+
+	/** Max Mana Amount. Capes Mana amount
+	 * Can be updated during runtime.
+	 * This variable is replicated.
+	 */
+	UPROPERTY(ReplicatedUsing = OnRep_MaxStamina, BlueprintReadOnly, Category = "Mana")
+	FGameplayAttributeData MaxStamina;
+	ATTRIBUTE_ACCESSORS(UGAS_AttributeSetBase, MaxStamina)
+
+	/** Increase stamina amount every second.
+	 * Can be updated during runtime
+	 * This variable is replicated.
+	 */
+	UPROPERTY(ReplicatedUsing = OnRep_StaminaRegenRate, BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData StaminaRegenRate;
+	ATTRIBUTE_ACCESSORS(UGAS_AttributeSetBase, StaminaRegenRate)
 	
 };
